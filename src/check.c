f7c13c36ed5260d78d8f96e0af1a0b8fb21c5d04
Nikulin Maxim <Flar1@users.noreply.github.com>
2023-05-26 03:35:29 +0700

    update

--- src/check.c
+++ src/check.c
@@ -6,7 +6,8 @@
 // new paths: ~/games/packman.cpp+/home/stud/alex/docs+~/study/Prog/lab4.c +/usr/bin/gcc
 
 #include <stdio.h>
-#include <mystrings.h>
+#include "mystrings.h"
+#include <stdlib.h>
 #define max_len 2000
 
 char* input(char* delim, char* dir)
@@ -29,6 +30,38 @@ char* input(char* delim, char* dir)
 
 char *process(char* delim, char *dir, char* paths)
 {
-    char *s = my_strtok(paths,delim);
-    
+    char *del_f = my_strtok(paths, delim);
+    char *new_paths = calloc(1024, sizeof(char));
+    char *delimiter = malloc(2 * sizeof(char));
+    delimiter[0] = delim;
+    delimiter[1] = '\0';
+    while (del_f != NULL)
+    {
+        if (del_f[0] == '~' && del_f[1] != '/')
+        {
+            char *p = malloc(1024 * sizeof(char));
+            my_strcat(p, dir);
+            del_f[0] = '/';
+            my_strcat(p, del_f);
+            my_strcat(p, delimiter);
+            my_strcat(new_paths, p);
+            free(p);
+        }
+        else
+        {
+            my_strcat(new_paths, del_f);
+            my_strcat(new_paths, delimiter);
+
+        }
+
+        del_f = my_strtok(NULL, delim);
+    }
+    if (my_strlen(new_paths) > 0 && new_paths[my_strlen(new_paths) - 1] == delim) {
+    new_paths[my_strlen(new_paths) - 1] = '\0';
+    }
+    printf("%s\n", paths);
+    free(del_f);
+    free(delimiter);
+    return new_paths;
 }
+    
--- src/test.c
+++ src/test.c
@@ -5,34 +5,49 @@
 #include <stdio.h>
 #include "mystrings.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include "mystrings.h"
+#include <stdlib.h>
+
 int main()
 {
     char s[1000] = "~/games/packman.cpp+~alex/docs+~/study/Prog/lab4.c+/usr/bin/gcc";
     char dir[1000] = "/home/stud";
     char delim = '+';
-    char *del_f= my_strtok(s,delim);
-    char *paths = calloc(1024,sizeof(char));
-    //printf("%s",dir);
+    char *del_f = my_strtok(s, delim);
+    char *paths = calloc(1024, sizeof(char));
+    char *delimiter = malloc(2 * sizeof(char));
+    delimiter[0] = delim;
+    delimiter[1] = '\0';
+
     while (del_f != NULL)
     {
         if (del_f[0] == '~' && del_f[1] != '/')
         {
-            printf("%ld\n",my_strlen(dir));
-            char *p = malloc(1024 *sizeof(char));
-            my_strcat(p,dir);
+            char *p = malloc(1024 * sizeof(char));
+            my_strcat(p, dir);
             del_f[0] = '/';
-            my_strcat(p,del_f);
-            printf("%s\n",p);
-            my_strcat(paths,p); 
+            my_strcat(p, del_f);
+            my_strcat(p, delimiter);
+            my_strcat(paths, p);
+            free(p);
         }
         else
         {
-            my_strcat(paths,del_f);
-        }
-    //printf("%s",paths);
-    //printf ("%s\n",del_f);
-    del_f = my_strtok (NULL,delim);
-   }
-   printf("%s\n",paths);
+            my_strcat(paths, del_f);
+            my_strcat(paths, delimiter);
 
+        }
+
+        del_f = my_strtok(NULL, delim);
+    }
+    if (my_strlen(paths) > 0 && paths[my_strlen(paths) - 1] == delim) {
+    paths[my_strlen(paths) - 1] = '\0';
+    }
+    printf("%s\n", paths);
+    free(del_f);
+    free(paths);
+    free(delimiter);
+
+    return 0;
 }
